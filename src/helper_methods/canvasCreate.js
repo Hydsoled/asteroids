a16a50cd0764925709a82cb6c33db623f063f0fd
hydsoled <dato_apple@yahoo.com>
2020-05-23 17:09:12 +0400

    finish project v1

--- src/helper_methods/canvasCreate.js
+++ src/helper_methods/canvasCreate.js
@@ -3,46 +3,6 @@ import Asteroids from "../objects/asteroids";
 import p5 from "p5";
 
 export default class CanvasCreate {
-    canvasSetup(ship, asteroids,action) {
-        let interaval = setInterval(()=>{
-            action.shipAppear = !action.shipAppear;
-        },100);
-        setTimeout(()=>{
-            action.blockHit = false;
-            action.shipAppear = true;
-            clearInterval(interaval);
-        },3000);
-        if (window.innerWidth < 450 && window.innerWidth !== s.width) {
-            s.createCanvas(window.innerWidth - 5, (window.innerWidth - 5) * 1.5);
-            ship.pos = s.createVector(s.width / 2, s.height / 2);
-            for (let i = 0; i < 5; i++) {
-                asteroids.push(new Asteroids(p5));
-            }
-            if (ship.r === 20) {
-                ship.r /= 1.4;
-                for (let i = asteroids.length; i >= 0; i--) {
-                    if (asteroids[i]) {
-                        asteroids[i].r /= 1.4;
-                    }
-                }
-            }
-        } else {
-            s.createCanvas(450, 600);
-            ship.pos = s.createVector(s.width / 2, s.height / 2);
-            for (let i = 0; i < 5; i++) {
-                asteroids.push(new Asteroids(p5));
-            }
-            if (ship.r !== 20) {
-                ship.r *= 1.4;
-                for (let i = asteroids.length; i >= 0; i--) {
-                    if (asteroids[i]) {
-                        asteroids[i].r *= 1.4;
-                    }
-                }
-            }
-        }
-    }
-
     canvasRes(ship, asteroids) {
         if (window.innerWidth < 450 && window.innerWidth !== s.width) {
             if (ship.r === 20) {
@@ -53,7 +13,7 @@ export default class CanvasCreate {
                     }
                 }
             }
-            s.resizeCanvas(window.innerWidth - 20, (window.innerWidth - 20) * 1.5);
+            s.resizeCanvas(window.innerWidth - 5, (window.innerWidth - 5) * 1.5);
         } else {
             if (ship.r !== 20) {
                 ship.r *= 1.4;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ObjectMovement from "./helper_methods/objectMovement";
 import Action from "./helper_methods/action";
 import CanvasCreate from "./helper_methods/canvasCreate";
 import AsteroidSpawn from "./helper_methods/asteroidSpawn";
+import canvasCreate from "./helper_methods/canvasCreate";
 let assets = require('./objects/assets');
 
 const sketch = (p) => {
@@ -17,15 +18,23 @@ const sketch = (p) => {
     let score = 0;
     let lives = 3;
     let img = 0;
+    let timer;
+    let spnTime;
+    let spnTimeSec;
     const keyboardHelper = new KeyboardHelper();
     let ship = new Ship(p, p5);
     const action = new Action(lives, score, ship);
     const createCanvas = new CanvasCreate();
     const asteroidSpawn = new AsteroidSpawn();
     p.setup = () => {
-        createCanvas.canvasSetup(ship,asteroids,action);
-        mc(ship,lasers, p5, action);
+        if (window.innerWidth < 450 && window.innerWidth !== s.width) {
+            s.createCanvas(window.innerWidth - 5, (window.innerWidth - 5) * 1.5);
+            ship.r/=1.4;
+        } else {
+            s.createCanvas(450, 600);
+        }
         p.resetFunction();
+        mc(ship,lasers, p5, action);
     };
     p.draw = () => {
         p.background(allAssets.background[img]);
@@ -40,6 +49,9 @@ const sketch = (p) => {
         if (p.keyCode === 32 && action.lives > 0) {
             lasers.push(new Laser(ship.pos, ship.heading, p5));
         }
+        if (p.keyCode === 32 && action.lives === 0) {
+            p.resetFunction();
+        }
     }
     p.keyReleased = () => {
         button[p.keyCode] = 0;
@@ -47,7 +59,7 @@ const sketch = (p) => {
     }
 
     p.windowResized = () => {
-        createCanvas.canvasRes(ship, asteroids);
+        createCanvas.canvasRes(ship,asteroids);
     };
     p.preload = () => {
         allAssets.background = [];
@@ -62,11 +74,26 @@ const sketch = (p) => {
         allAssets.ship.push(s.loadImage(assets.ship[0]));
     }
     p.resetFunction = () =>{
-        let spnTimeSec = 4000;
-        let spnTime = setInterval(()=>{
+        action.lives = 3;
+        action.score = 0;
+        asteroids = [];
+        img = 0;
+        let interaval = setInterval(()=>{
+            action.shipAppear = !action.shipAppear;
+        },100);
+        setTimeout(()=>{
+            action.blockHit = false;
+            action.shipAppear = true;
+            clearInterval(interaval);
+        },3000);
+        ship.pos = s.createVector(s.width / 2, s.height / 2);
+        spnTimeSec = 4000;
+        clearInterval(spnTime);
+        clearInterval(timer);
+        spnTime = setInterval(()=>{
             asteroidSpawn.spawn(asteroids,p5,ship);
         },spnTimeSec);
-        let timer = setInterval(()=> {
+        timer = setInterval(()=> {
             if (spnTimeSec>2000) {
                 spnTimeSec-=1000;
                 if (assets.background[img+1]){
@@ -78,7 +105,7 @@ const sketch = (p) => {
             spnTime = setInterval(()=>{
                 asteroidSpawn.spawn(asteroids,p5,ship);
             },spnTimeSec);
-        },60000);
+        },10000);
 
     }
 };
